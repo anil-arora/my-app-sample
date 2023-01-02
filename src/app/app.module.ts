d74b680a2639463f65647ee0cec093cc8330d212
Anshu Arora <anshuarora8919@gmail.com>
2023-01-02 15:14:20 +0530

    second commited

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { TestComponent } from './test/test.component';
 import { ProductComponent } from './product/product.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 
 @NgModule({
@@ -15,6 +16,7 @@ import { TopBarComponent } from './top-bar/top-bar.component';
     ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: ProductComponent },
+      { path: 'products/:productId', component: ProductDetailsComponent },
     ])
     ],
   declarations: [
@@ -23,6 +25,7 @@ import { TopBarComponent } from './top-bar/top-bar.component';
     ProductComponent,
     ProductAlertsComponent,
     TopBarComponent,
+    ProductDetailsComponent,
     ],
   
   providers: [],
